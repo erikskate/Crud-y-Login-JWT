5f25b9d59c708c8e60294d55be125ebcba4d598b
erikskate <erikjoel35@hotmail.com>
2022-11-03 12:24:19 -0600

    Se agregan metodos en JwtEntryPoint, JwtProvider y JwtTokenFilter

--- src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,63 @@
 package com.tutorial.crud.security.jwt;
 
-public class JwtTokenFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtTokenFilter extends OncePerRequestFilter{
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try{
+
+            String token = getToken(req);
+            if(token != null && jwtProvider.validateToken(token)){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(nombreUsuario);
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+            }
+
+        }catch(Exception e){
+            logger.error("Fallo el metodo doFilter",e);
+        }
+
+        filterChain.doFilter(req, res);
+        
+    }
+
+    private String getToken(HttpServletRequest req){
+        String header = req.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer")){
+            return header.replace("Bearer ", "");
+        }
+        return null;
+    }
     
 }
