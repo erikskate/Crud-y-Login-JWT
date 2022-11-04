fa2a47c4d34f38d533168416c610ea91cdfbe48b
erikskate <erikjoel35@hotmail.com>
2022-11-04 11:51:49 -0600

    Se agrega documentación con swagger y verificar la firma del token

--- src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
@@ -39,7 +39,7 @@ public class JwtProvider {
                     .setSubject(usuarioPrincipal.getUsername())
                     .claim("roles", roles)
                     .setIssuedAt(new Date())
-                    .setExpiration(new Date(new Date().getTime() + expiration))
+                    .setExpiration(new Date(new Date().getTime() + expiration * 180))
                     .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                     .compact();
     }
@@ -68,17 +68,22 @@ public class JwtProvider {
 
     public String refreshToken(JwtDto jwtDto) throws ParseException{
 
-        JWT jwt = JWTParser.parse(jwtDto.getToken());
-        JWTClaimsSet claims = jwt.getJWTClaimsSet();
-        String nombreUsuario = claims.getSubject();
-        List<String> roles = (List<String>) claims.getClaim("roles");
-
-        return Jwts.builder()
-                    .setSubject(nombreUsuario)
-                    .claim("roles", roles)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(new Date().getTime() + expiration))
-                    .signWith(SignatureAlgorithm.HS512, secret.getBytes())
-                    .compact();
+        try{
+            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(jwtDto.getToken());
+        }catch(ExpiredJwtException e){
+            JWT jwt = JWTParser.parse(jwtDto.getToken());
+            JWTClaimsSet claims = jwt.getJWTClaimsSet();
+            String nombreUsuario = claims.getSubject();
+            List<String> roles = (List<String>) claims.getClaim("roles");
+    
+            return Jwts.builder()
+                        .setSubject(nombreUsuario)
+                        .claim("roles", roles)
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(new Date().getTime() + expiration * 180))
+                        .signWith(SignatureAlgorithm.HS512, secret.getBytes())
+                        .compact();
+        }
+       return null;
     }
 }
